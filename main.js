52b8f3da456158215c808b75ef9705596d1a885a
Yossab_Samuel <yossabpro5@gmail.com>
2024-04-24 14:14:35 +0200

    end html css files

---
+++ main.js
@@ -0,0 +1,7 @@
+let textPDiv = document.querySelectorAll(".text p");
+let titleDiv = document.querySelectorAll(".landing .text .title");
+let imageDiv = document.querySelectorAll(".landing .photo .pic");
+console.log()
+textPDiv[1].remove()
+titleDiv[1].remove();
+imageDiv[1].remove();
